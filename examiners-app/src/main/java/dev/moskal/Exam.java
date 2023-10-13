ac5e205aa2ab05feca309818e0e1bf5e75797e11
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2023-10-13 21:45:46 +0200

    Updated Exam.java

--- examiners-app/src/main/java/dev/moskal/Exam.java
+++ examiners-app/src/main/java/dev/moskal/Exam.java
@@ -1,7 +1,13 @@
 package dev.moskal;
 
+import java.util.Date;
 import lombok.Data;
 
+@Data
 public class Exam {
-	
+	private final String id;
+	private final String category;
+	private final Date date;
+	private final String result;
+	private final String otherDetails;
 }
