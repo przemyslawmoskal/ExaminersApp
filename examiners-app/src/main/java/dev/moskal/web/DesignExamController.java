38d05e30245d44a25fe876ecb70b69eb145103d6
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2023-10-16 00:37:21 +0200

    Created design.html and added funcion in DesignExamController.java

--- examiners-app/src/main/java/dev/moskal/web/DesignExamController.java
+++ examiners-app/src/main/java/dev/moskal/web/DesignExamController.java
@@ -2,6 +2,7 @@ package dev.moskal.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -15,4 +16,9 @@ public class DesignExamController {
 	public void addExamToModel(Model model) {
 		
 	}
+	
+	@GetMapping
+	 public String showDesignForm() {
+		return "design";
+	 }
 }
